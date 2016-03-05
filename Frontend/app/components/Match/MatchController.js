8f610f84bbdd9ceb45be79979cd5f83200807d86
Manfred Rosskamp <manfred.rosskamp@gmx.de>
2016-03-05 13:53:06 +0100

    update match

--- Frontend/app/components/Match/MatchController.js
+++ Frontend/app/components/Match/MatchController.js
@@ -43,10 +43,66 @@ app.controller('MatchesCtrl', function ($scope, $rootScope, $http, SettingsServi
 		loadTable();
 });
 
-app.controller('MatchDetailsCtrl', function ($scope, $routeParams, $http, SettingsService) {
-	$http.get(SettingsService.backPrefix + 'matches?filter=id,eq,' + $routeParams.matchId).then(function (matchResp) {
-		$scope.m = php_crud_api_transform(matchResp.data)[SettingsService.tablePrefix + 'matches'][0];
-
-		// get saison teams
+app.controller('MatchDetailsCtrl', function ($q, $scope, $rootScope, $routeParams, $http, SettingsService, DataService) {
+	$scope.$on("ckeditor.ready", function (event) {
+		$scope.isReady = true;
 	});
+
+	$http.get(SettingsService.backPrefix + 'match?filter=id,eq,' + $routeParams.matchId).then(function (matchResp) {
+		$scope.m = php_crud_api_transform(matchResp.data)[SettingsService.tablePrefix + 'match'][0];
+	});
+	
+	var loadSaisonTeams = function () {
+		if ($rootScope.selectedSaison == null)
+			return;
+		
+		var teamPromise = DataService.getTeams(true);
+		var saisonTeamPromise = $http.get(SettingsService.backPrefix 
+			+ 'saison_team?filter=id_saison,eq,' 
+			+ $rootScope.selectedSaison.id);
+
+		$q.all([saisonTeamPromise, teamPromise]).then(function (results) {
+
+			var teams = results[1];
+
+			var saisonTeams = php_crud_api_transform(results[0].data)[SettingsService.tablePrefix + 'saison_team'];
+			
+			for (var i = 0; i < saisonTeams.length; i++) {
+				saisonTeams[i].team = teams[saisonTeams[i].id_team];
+			}
+			$scope.saisonTeams = saisonTeams;
+		});
+	};
+
+	$rootScope.$watch('selectedSaison', loadSaisonTeams);
+
+	if ($rootScope.selectedSaison != null)
+		loadSaisonTeams();
+
+
+	$scope.save = function () {
+		//$scope.article.title = $scope.title;
+		$scope.error = null;
+
+		if (!$scope.frm.$dirty) {
+			var directP = $q.defer();
+			setTimeout(function () { directP.resolve(true); }, 0);
+			return directP;
+		}
+
+		var match = $scope.m;
+
+		if ($scope.frm.editor.$dirty) {
+			data.content = $scope.editorContent;
+		}
+
+		return $http({ "method": method, "url": url, "data": data })
+			.then(function success(response) {
+				if ($routeParams.articleId == 'new') {
+					$location.path('/EditArticle/' + response.data);
+				}
+			}, function failed(response) {
+				$scope.error = response.statusText + ": " + response.data;
+			});
+	}
 });
